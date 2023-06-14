fc98b9e32d5a5d7ba8d521378c014cf490312c83
michelle316 <38562417+MichelleHS777@users.noreply.github.com>
2023-06-14 17:02:32 +0800

    230614

---
+++ preprocess_.py
@@ -0,0 +1,71 @@
+from tqdm import tqdm
+import re
+import json
+
+def preprocess_predict(unpreprocess_file, save_file):
+    dataset = json.load(open(unpreprocess_file, 'r', encoding='utf-8'))
+    save_file = open(save_file, 'w', encoding='utf-8')
+    for data in tqdm(dataset, desc='Preprocess to predict file'):
+        claimId = data['claimId']
+        claim = data['claim']
+        label = data['label']
+        sentences = []
+        # get the gold evidence
+        gold_evidences = [data['gold evidence'][str(i)]['text'] for i in range(5) if data['gold evidence'][str(i)]['text'] != '']
+        # split document into sentences
+        doc = [data['evidence'][str(i)]['text'] for i in range(5) if data['evidence'][str(i)]['text'] != '']
+        for text in doc:
+            # remove gold evidence in document
+            for gold_evidence in gold_evidences:
+                text = text.replace(gold_evidence, '')
+            # split the sentence with punctuation
+            text = re.split(r'[？：。！（）.“”…\t\n]', text) 
+            # get the sentences: not none and len > 5
+            text = [evidences for evidences in text
+                            if evidences != '' and len(evidences) > 5]
+            sentences.extend(text)
+        each_data = {'claimId': int(claimId), 'claim': claim, 'evidences': gold_evidences + sentences, 'label': label}
+        data = json.dumps(each_data, ensure_ascii=False)
+        save_file.write(data + "\n")
+    save_file.close()
+
+def preprocess_train(unpreprocess_file, save_file):
+    dataset = json.load(open(unpreprocess_file, 'r', encoding='utf-8'))
+    save_file = open(save_file, 'w', encoding='utf-8')
+    for data in tqdm(dataset, desc='Preprocess training data'):
+        # write claim in file
+        claim = data['claim']
+        save_file.write(claim + "\n") 
+
+        # write gold evidence in file
+        gold_evidences = [data['gold evidence'][str(i)]['text'] for i in range(5) if data['gold evidence'][str(i)]['text'] != '']
+        for gold in gold_evidences:
+            save_file.write(gold + "\n")
+        
+        # split document into sentences
+        sentences = []
+        doc = [data['evidence'][str(i)]['text'] for i in range(5) if data['evidence'][str(i)]['text'] != '']
+        for text in doc:
+            # remove gold evidence in document
+            for gold_evidence in gold_evidences:
+                text = text.replace(gold_evidence, '')
+            # split the sentence with punctuation
+            text = re.split(r'[？：。！（）.“”…\t\n]', text) 
+            # get the sentences
+            text = [evidences for evidences in text
+                    if evidences != '' and len(evidences) > 5]
+            sentences.extend(text)
+        # write sentences in file
+        for sent in sentences:
+            save_file.write(sent + "\n")
+    save_file.close()
+    
+
+def main():
+    preprocess_train('./datasets/unpreprocess/train.json', 
+                       './datasets/train/train.data')
+    preprocess_predict('./datasets/unpreprocess/test.json', 
+                       './datasets/predict/test.json')
+
+if __name__ == "__main__":
+    main()
