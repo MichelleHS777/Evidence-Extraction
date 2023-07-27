cd31198239a07ad582322d90466ec6583b98e052
michelle316 <38562417+MichelleHS777@users.noreply.github.com>
2023-07-27 22:34:04 +0800

    Delete run.sh

--- run.sh
+++
@@ -1,7 +0,0 @@
-#python main.py --eval --test_data_path='datasets/preprocessed/train.json' --save_file='datasets/evidence/promptbert/prompt_train2.json'
-#python main.py --eval --test_data_path='datasets/preprocessed/dev.json' --save_file='datasets/evidence/promptbert/prompt_dev2.json'
-#python main.py --eval --test_data_path='datasets/preprocessed/test.json' --save_file='datasets/evidence/promptbert/prompt_test2.json'
-
- python Semantic_Ranker.py  --test_data_path='datasets/preprocessed/train.json' --save_file='datasets/evidence/semantic/semantic_train2.json'
- python Semantic_Ranker.py  --test_data_path='datasets/preprocessed/dev.json' --save_file='datasets/evidence/semantic/semantic_dev2.json'
- python Semantic_Ranker.py  --test_data_path='datasets/preprocessed/dev.json' --save_file='datasets/evidence/semantic/semantic_dev2.json'
