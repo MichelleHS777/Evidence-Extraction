174af715596449cc61c1b3e4fd3b22124df70657
michelle316 <38562417+MichelleHS777@users.noreply.github.com>
2023-08-05 13:05:58 +0800

    Update document_retrieval.py

--- PromptBERT/document_retrieval.py
+++ PromptBERT/document_retrieval.py
@@ -6,15 +6,16 @@ import json
 parser = argparse.ArgumentParser(description='Document Retrieval')
 parser.add_argument('--save_file', type=str, default='doc_covid.json', help='save document result from google search')
 parser.add_argument('--claim', type=str, default="COVID-19疫苗研發突破：輝瑞與BioNTech合作疫苗獲得顯著效果", help='enter a claim that you want to search')
-parser.add_argument('--start_date', type=str, default="2020-01-01", help='starting date')
-parser.add_argument('--end_date', type=str, default="2020-12-31", help='end date')
+# parser.add_argument('--start_date', type=str, default="2020-01-01", help='starting date')
+# parser.add_argument('--end_date', type=str, default="2020-12-31", help='end date')
 args = parser.parse_args()
 
 save = open(args.save_file, 'a+', encoding='utf-8')
 claim = args.claim
 start_date = args.start_date
 end_date = args.end_date
-query = claim + " after:" + start_date + " before:" + end_date
+query = claim
+# query = claim + " after:" + start_date + " before:" + end_date
 results = search(query, stop=10, pause=2.0, lang="zh-tw")
 
 # save search results 
