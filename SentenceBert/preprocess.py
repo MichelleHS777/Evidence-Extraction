a10df3447e8cac75d2a1b41bd9d06657216eda22
michelle316 <38562417+MichelleHS777@users.noreply.github.com>
2023-08-05 13:45:58 +0800

    Update preprocess.py

--- SentenceBert/preprocess.py
+++ SentenceBert/preprocess.py
@@ -6,7 +6,7 @@ import argparse
 # ------------------------init parameters----------------------------
 parser = argparse.ArgumentParser(description='Preprocess Datasets')
 parser.add_argument('--dataset', type=str, default="'./datasets/test.json', help='dataset path')
-parser.add_argument('--save_file', type=str, default='datasets/test.json', help='save file path')
+parser.add_argument('--save_file', type=str, default='datasets/predict/test.json', help='save file path')
 
 args = parser.parse_args()
 dataset = json.load(open(args.dataset, 'r', encoding='utf-8'))
